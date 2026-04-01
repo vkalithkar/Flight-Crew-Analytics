79fb76b0be7191734f0a025475f664a15872034c
Katy D. Lenshin <kdlenshin@bora.sciclone.wm.edu>
2026-03-31 23:08:14 -0400

    finalizing functionality to download data from NASA website -- perhaps can be commented more, but otherwise it works.

--- scripts/download_data.sh
+++ scripts/download_data.sh
@@ -4,19 +4,23 @@ WEBSITE_LOC="data-site.htm"
 
 cd data
 
-# trying to get all of the zip files from nasa data website (html parsing)
-#for line in $(grep -Po 'https://[^" >]+\.zip' $WEBSITE_LOC); do wget -P raw/ $line;>
-#for line in $(grep -Po 'https://[^" >]+\.zip' $WEBSITE_LOC); do echo $line;done
-
-grep -Po 'https://[^" >]+\.zip' "$WEBSITE_LOC" | wget -c --show-progress -P raw/ -i -
-
+# getting the flight crew CSV zip files from nasa data website (html parsing)
+grep -Po 'https://[^" >]+\.zip' "$WEBSITE_LOC" | while read line; do 
+	wget -c --show-progress \
+	--wait=15 \
+	--random-wait \
+	--timeout=60 \
+	-P raw/ "$line"
+done
 
 cd raw/ 
 
 # unzipping all of those files
 for file in *.zip; do unzip $file; done
 
+# clean up
 rm *.zip
 
+# return to root
 cd ..
 cd ..
